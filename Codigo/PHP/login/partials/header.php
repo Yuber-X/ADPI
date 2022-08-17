e07db5d2426e4f1f8c74ec3087103e7997fa760d
YuberKill <yuberminecraft@gmail.com>
2022-08-17 01:27:37 -0400

    Menos peso

---
+++ Codigo/PHP/login/partials/header.php
@@ -0,0 +1,3 @@
+<header>
+        <a href="/login">ADPI</a>
+    </header>
---
+++ Codigo/PHP/login/php.php
@@ -0,0 +1,11 @@
+<?php
+
+foreach ($valores[id] as &$valor) {
+  echo   
+}
+
+?>
+
+<select name="" id="">
+<option value=""><?php $value. ?></option>
+</select>
