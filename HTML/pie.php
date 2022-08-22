f8750100f80f009afa572cd359f7f3daebfd12d4
TigerXV <stevenmanuel2009@gmail.com>
2022-08-22 01:20:27 -0400

    Footer realizado y encapsulacion hecha pie.php

---
+++ HTML/pie.php
@@ -0,0 +1,50 @@
+<footer class="footer-container">
+    <div class="footer-container-caja-top">
+      <div class="footer-container-caja-top__soporte">
+        <h3>Soporte</h3>
+        <div>
+          <a href="#">Contacto</a>
+          <a href="#">Cuenta</a>
+          <a href="#">Terminos y condiciones</a>
+          <a href="#">Preguntas Frecuentes</a>
+        </div>
+      </div>
+
+
+      <div class="footer-container-caja-top__adm">
+        <h3>Administrador</h3>
+        <div>
+          <a href="#">Ingresar</a>
+        </div>
+
+      </div>
+
+      <div class="footer-container-caja-top__redesSociales">
+
+        <div>
+          <p class="footer-container-caja-top_redesSociales__logo">ADPI</p>
+        </div>
+
+        <div>
+
+          <p class="footer-container-caja-top_redesSociales__siguenos">Siguenos: </p>
+
+          <div class="footer-container-caja-top_redesSociales__TIF">
+            <img src="./img/instagram.png">
+            <img src="./img/twitter.png">
+            <img src="./img/facebook.png">
+          </div>
+        </div>
+
+      </div>
+
+
+    </div>
+
+    <div class="footer-container-caja-buttom">
+      <img src='./img/copyright adpi.PNG'>
+      <img src='./img/paypal.png'>
+      <img src='./img/Former_Visa.png'>
+    </div>
+
+  </footer>
